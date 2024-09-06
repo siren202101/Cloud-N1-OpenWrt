9f4084f6d779a2d7c146a874f2dcd17bc4adc5f9
siren202101 <80493699+siren202101@users.noreply.github.com>
2024-09-06 18:51:31 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -14,6 +14,8 @@ mv -f theme-temp/luci-theme-opentomcat package/lean/
 rm -rf theme-temp
 default_theme='opentomcat'
 sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
+### 添加第三方订阅源
+sed -i '$a src-git kiddin9_openwrt_packages https://github.com/kiddin9/openwrt-packages.git' feeds.conf.default
 
 # Add luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic.git  package-temp/luci-app-amlogic
