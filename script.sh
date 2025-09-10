0cab9fccc4726b8e0f23395b878abe87f58f708b
siren202101 <80493699+siren202101@users.noreply.github.com>
2025-09-10 18:11:01 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -16,8 +16,10 @@ default_theme='opentomcat'
 sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
 ### 添加第三方订阅源
 #sed -i '$a src-git kiddin9_openwrt_packages https://github.com/kiddin9/openwrt-packages.git' feeds.conf.default
-sed -i '$a src-git kenzok8_openwrt_packages https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
-sed -i '$a src-git kenzok8_small_package https://github.com/kenzok8/small-package.git' feeds.conf.default
+###sed -i '$a src-git kenzok8_openwrt_packages https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
+###sed -i '$a src-git kenzok8_small_package https://github.com/kenzok8/small-package.git' feeds.conf.default
+sed -i '1i src-git kenzok8_openwrt_packages https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
+sed -i '1i src-git kenzok8_small_package https://github.com/kenzok8/small-package.git' feeds.conf.default
 
 ###修改IP###
 sed -i 's/192.168.1.1/192.168.88.251/g' package/base-files/files/bin/config_generate
