381ee17a8e1eda89e5cbc25774e165c2cf2d608d
siren202101 <80493699+siren202101@users.noreply.github.com>
2025-12-14 20:18:57 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -21,8 +21,8 @@ mv -f package-temp/luci-app-amlogic/luci-app-amlogic package/lean/
 rm -rf package-temp
 
 ###修改luci分支###
-sed -i 's/src-git luci https:\/\/github.com\/coolsnowwolf\/luci.git;openwrt-23.05/#src-git luci https:\/\/github.com\/coolsnowwolf\/luci.git;openwrt-23.05/' feeds.conf.default
-sed -i 's/#src-git luci https:\/\/github.com\/coolsnowwolf\/luci.git;openwrt-24.10/src-git luci https:\/\/github.com\/coolsnowwolf\/luci.git;openwrt-24.10/' feeds.conf.default
+#sed -i 's/src-git luci https:\/\/github.com\/coolsnowwolf\/luci.git;openwrt-23.05/#src-git luci https:\/\/github.com\/coolsnowwolf\/luci.git;openwrt-23.05/' feeds.conf.default
+#sed -i 's/#src-git luci https:\/\/github.com\/coolsnowwolf\/luci.git;openwrt-24.10/src-git luci https:\/\/github.com\/coolsnowwolf\/luci.git;openwrt-24.10/' feeds.conf.default
 
 ###修改IP###
 sed -i 's/192.168.1.1/192.168.88.8/g' package/base-files/files/bin/config_generate
